8299a91819b4a62c6df0fcfb14e655a53dbe2cba
AhmedShowmik <ashowmik1@gmail.com>
2026-09-06 19:20:11 +0600

    Add demo C# demo2 code

---
+++ demo2.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace LoginSystem
+{
+    class UserAccount
+    {
+        private string username;
+        private string password;
+
+        public UserAccount(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public bool Login(string enteredUsername, string enteredPassword)
+        {
+            return username == enteredUsername && password == enteredPassword;
+        }
+
+        public void ShowUserInfo()
+        {
+            Console.WriteLine("User: " + username);
+            Console.WriteLine("Account Status: Active");
+        }
+    }
+
+    class DemoLogin
+    {
+        static void Main()
+        {
+            UserAccount user = new UserAccount("student01", "pass123");
+
+            Console.WriteLine("=== Login System Demo ===");
+
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            if (user.Login(username, password))
+            {
+                Console.WriteLine("\nLogin Successful!");
+                user.ShowUserInfo();
+            }
+            else
+            {
+                Console.WriteLine("\nLogin Failed!");
+                Console.WriteLine("Invalid username or password.");
+            }
+        }
+    }
+}
